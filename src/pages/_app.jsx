e983eb7e03d0f1ca363db07128beca9c94894f72
rakanugraha18 <rakanugraha2@gmail.com>
2023-11-26 13:30:40 +0700

    Fix navbar and footer

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,13 @@
+import Footer from "@/components/organisms/Footer";
+import Header from "@/components/organisms/Header";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />;
+      <Footer />
+    </>
+  );
 }
